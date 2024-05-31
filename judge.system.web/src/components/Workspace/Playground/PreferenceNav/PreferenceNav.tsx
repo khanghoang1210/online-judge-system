acbad3caa08569b93556265f370caccbed79b466
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-05-31 17:49:53 +0700

    fix dropdown

--- judge.system.web/src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ judge.system.web/src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   AiOutlineFullscreen,
   AiOutlineFullscreenExit,
   AiOutlineSetting,
+  AiOutlineCheck,
 } from "react-icons/ai";
 import { ISettings } from "../Playground";
 import SettingsModal from "@/components/Modals/SettingsModal";
@@ -17,6 +18,9 @@ const PreferenceNav: React.FC<PreferenceNavProps> = ({
   settings,
 }) => {
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState("C++");
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleFullScreen = () => {
     if (isFullScreen) {
@@ -43,18 +47,39 @@ const PreferenceNav: React.FC<PreferenceNavProps> = ({
       document.addEventListener("MSFullscreenChange", exitHandler);
     }
   }, [isFullScreen]);
-  
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  const handleLanguageSelect = (language: string) => {
+    setSelectedLanguage(language);
+    setIsDropdownOpen(false);
+  };
+
+  const languages = ["C++", "Python", "Java"];
 
   return (
-    <div className="flex items-center justify-between bg-dark-layer-2 h-11 w-full ">
+    <div className="flex items-center justify-between bg-dark-layer-2 h-11 w-full relative">
       <button
         id="dropdownHoverButton"
-        data-dropdown-toggle="dropdownHover"
-        data-dropdown-trigger="hover"
-        className="flex cursor-pointer items-center rounded focus:outline-none bg-dark-fill-3 text-dark-label-2 hover:bg-dark-fill-2  px-2 py-1.5 font-medium"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        className="flex cursor-pointer items-center rounded focus:outline-none bg-dark-fill-3 text-dark-label-2 hover:bg-dark-fill-2 px-2 py-1.5 font-medium"
         type="button"
       >
-        C++
+        {selectedLanguage}
         <svg
           className="w-2.5 h-2.5 ms-3"
           aria-hidden="true"
@@ -64,48 +89,41 @@ const PreferenceNav: React.FC<PreferenceNavProps> = ({
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="m1 1 4 4 4-4"
           />
         </svg>
       </button>
 
-      <div
-        id="dropdownHover"
-        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-      >
-        <ul
-          className="py-2 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownHoverButton"
+      {isDropdownOpen && (
+        <div
+          id="dropdownHover"
+          ref={dropdownRef}
+          className="z-10 bg-neutral-700 divide-y divide-gray-100 rounded-lg shadow w-44 absolute top-11 left-0 "
         >
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Python
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Java
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      {/* <div className='flex items-center text-white'>
-				<button className='flex cursor-pointer items-center rounded focus:outline-none bg-dark-fill-3 text-dark-label-2 hover:bg-dark-fill-2  px-2 py-1.5 font-medium'>
-					<div className='flex items-center px-1'>
-						<div className='text-xs text-label-2 dark:text-dark-label-2'>JavaScript</div>
-					</div>
-				</button>
-			</div> */}
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-gray-200 "
+            aria-labelledby="dropdownHoverButton"
+          >
+            {languages.map((language) => (
+              <li key={language}>
+                <a
+                  href="#"
+                  onClick={() => handleLanguageSelect(language)}
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-dark-fill-3 dark:hover:text-white flex justify-between"
+                >
+                  {language}
+                  {selectedLanguage === language && (
+                    <AiOutlineCheck className="text-white-500" />
+                  )}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       <div className="flex items-center m-2">
         <button
