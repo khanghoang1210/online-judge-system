d2b3e3a0d8fb832a24788a2ce56de0a5cf1c6ab3
Dangkhoi14 <dangkhoi111003@gmail.com>
2024-06-02 11:31:01 +0700

    Almost complete Get N Nearest Submissions

--- judge.system.core/Service/Impls/SubmissionService.cs
+++ judge.system.core/Service/Impls/SubmissionService.cs
@@ -3,7 +3,6 @@ using judge.system.core.Database;
 using judge.system.core.DTOs.Responses;
 using judge.system.core.DTOs.Responses.Submission;
 using judge.system.core.Service.Interface;
-using Microsoft.EntityFrameworkCore;
 
 namespace judge.system.core.Service.Impls
 {
@@ -20,20 +19,14 @@ namespace judge.system.core.Service.Impls
 
         public async Task<APIResponse<List<GetSubmissionRes>>> GetNNearest(string UserName, int size)
         {
-            var submissions = await _context.Submissions.ToListAsync();
-            List<GetSubmissionRes> res = new List<GetSubmissionRes>();
+            var submissions = await _context.Submissions
+                .Where(x => x.UserName == UserName)
+                .OrderByDescending(x => x.TIME) // Time Submited
+                .Take(size)
+                .ToListAsync();
 
 
-            foreach (var item in submissions)
-            {
-                var submission = _mapper.Map<GetSubmissionRes>(item);
-                res.Add(submission);
-                if (size == 1)
-                {
-                    break;
-                }
-                size--;
-            }
+            var res = _mapper.Map<List<GetSubmissionRes>>(submissions);
 
             return new APIResponse<List<GetSubmissionRes>>
             {
