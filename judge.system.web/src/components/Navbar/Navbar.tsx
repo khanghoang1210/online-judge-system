6e09873330bb485c0ee1a76edbeab7f5520fb7fe
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-07-05 15:26:16 +0700

    add something

--- judge.system.web/src/components/Navbar/Navbar.tsx
+++ judge.system.web/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { authModalState } from '@/atoms/authModalAtom';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
@@ -16,7 +17,7 @@ const Navbar:React.FC<NavbarProps> = () => {
     
     return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
         <Link href="/" className="flex items-center justify-center h-20">
-            <img src="/logo.png" width={"150"} height={"100"} alt=""/>
+            <Image src="/logo.png" width={"150"} height={"100"} alt=""/>
         </Link>
         <div className='flex items-center'>
             <button className='bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium 
